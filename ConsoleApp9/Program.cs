3fb47c74a1c629d961a87c5d0b5581edbd53295f
WXJ <ecyxw1@nottingham.edu.my>
2018-11-01 20:58:37 +0800

    Added files

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            var b = ";
+            Console.WriteLine($"Hello World!");
+            Console.WriteLine(a);
+
+        
+        }
+    }
+}
